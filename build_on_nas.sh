fc351005596ce465d0985f410bb9235e33c4f257
asherqu <asherqu@tencent.com>
2026-07-07 13:33:57 +0800

    first commit

--- build_on_nas.sh
+++ build_on_nas.sh
@@ -273,6 +273,6 @@ green " 内含 aarch64 PyInstaller 后端二进制，运行时无需系统 pytho
 green " 安装: appcenter-cli install-fpk ${FPK}   或在飞牛应用中心手动上传安装"
 green "======================================================================"
 
-cd PKG_DIR
+cd "${PKG_DIR}"
 
 appcenter-cli install-local
